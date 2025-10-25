4a555cbc668cca24b381ecfb840e3fb1ceb29166
shahd <rtshahd84@gmail.com>
2025-10-25 16:53:06 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Empath_AI',
+  webDir: 'www'
+};
+
+export default config;
